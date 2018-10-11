74e5e23251f9e11f45eb24cc30d0341f52af7efa
Anthony LAMOUR <a.lamour@ludus-academie.com>
2018-10-11 14:28:44 +0200

    Code_Initiale

---
+++ Source/IntroductionUE4/IntroductionUE4GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IntroductionUE4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTRODUCTIONUE4_API AIntroductionUE4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
